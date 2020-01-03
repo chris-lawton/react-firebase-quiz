2e9c0a29311208f9587268bce2c7d4cdf8a6c3f6
Chris Lawton <chris.lawton@torchbox.com>
2020-01-03 16:27:45 +0000

    3

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function home() {
+    return (
+        <div className="container">
+            <h1>Quiz App</h1>
+            <a href="/game" className="btn">Start Game</a>
+            <a href="/game" className="btn">High Scores</a>
+        </div>
+    )
+}
