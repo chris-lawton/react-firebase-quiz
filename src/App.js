1671aeb6c25ee5d1e240de88ac31a807ea787cc6
Chris Lawton <chris.lawton@torchbox.com>
2020-01-03 16:40:05 +0000

    4 - routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Home from './components/Home';
+import Game from './components/Game';
+import HighScores from './components/HighScores';
+
 import './App.css';
-import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="container">
+        <Route exact path="/" component={Home} />
+        <Route path="/highScores" component={HighScores} />
+        <Route path="/game" component={Game} />
+      </div>
+    </Router>
   );
 }
 
