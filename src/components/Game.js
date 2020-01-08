13a3584ca0171f1ae559337eae2f715d7ed7c200
Chris Lawton <chris.lawton@torchbox.com>
2020-01-08 10:48:11 +0000

    add logic for changing questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,18 +8,38 @@ export default class Game extends Component {
         super(props);
         this.state = {
             questions: null,
-            currentQuestion: null,
-            loading: true
         };
     }
 
     async componentDidMount() {
         try {
             const questions = await loadQuestions();
-            this.setState({questions, currentQuestion: questions[0], loading: false})
+            this.setState({
+                questions,
+            }, () => {
+                // ensure questions is populated before calling changeQuestion()
+                this.changeQuestion();
+            })
         } catch (error) {
             console.log(error);
         }
+    }
+
+    changeQuestion = () => {
+        // get a random index of a question
+        const randomQuestionIndex = Math.floor(Math.random() * this.state.questions.length);
+
+        // set the current question to the question at that random index
+        const currentQuestion = this.state.questions[randomQuestionIndex]
+
+        // remove that question from the questions going forward
+        const remainingQuestions = [...this.state.questions];
+
+        // start at the random question index and remove one from the array
+        remainingQuestions.splice(randomQuestionIndex, 1);
+
+        // update state to reflect the changes
+        this.setState({questions: remainingQuestions, currentQuestion, loading: false})
     }
 
     render() {
