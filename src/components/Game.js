a131616332f84b88e7789a5bd00b24fc2f629308
Chris Lawton <chris.lawton@torchbox.com>
2020-01-08 17:18:37 +0000

    check when the user has completed all of the questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,6 +11,7 @@ export default class Game extends Component {
             questions: null,
             score: 0,
             questionNumber: 0,
+            done: false,
         };
     }
 
@@ -29,6 +30,9 @@ export default class Game extends Component {
     }
 
     changeQuestion = (bonus = 0) => {
+        // don't change question if the user is on the last question
+        if (this.state.questions.length === 0) return this.setState({done: true})
+
         // get a random index of a question
         const randomQuestionIndex = Math.floor(Math.random() * this.state.questions.length);
 
@@ -54,8 +58,8 @@ export default class Game extends Component {
     render() {
         return (
             <>
-                {this.state.loading && <div id="loader" />}
-                {!this.state.loading && this.state.currentQuestion &&
+                {this.state.loading && !this.state.done && <div id="loader" />}
+                {!this.state.done && !this.state.loading && this.state.currentQuestion &&
                     <>
                         <HUD score={this.state.score} questionNumber={this.state.questionNumber} />
                         <Question
@@ -64,6 +68,7 @@ export default class Game extends Component {
                         />
                     </>
                 }
+                {this.state.done && <p>Done</p>}
             </>
         )
     }
