94466ea2d68f53bce07b32bcfbc2d725543ab0f4
Chris Lawton <chris.lawton@torchbox.com>
2020-03-21 12:35:31 +0000

    add high scores

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useFirebase } from './firebase/FirebaseContext'
 
 export default function HighScores() {
+    const firebase = useFirebase();
+    const [scores, setScores] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        firebase.scores().once('value', snapshot => {
+            const data = snapshot.val();
+            const sortedScores = formatScoreData(data);
+            setScores(sortedScores);
+            setLoading(false);
+        });
+    }, [firebase])
+
+    const formatScoreData = firebaseScores => {
+        const scores = [];
+
+        for (let key in firebaseScores) {
+            const val = firebaseScores[key];
+            val['key'] = key;
+            scores.push(val);
+        }
+
+        return scores.sort((score1, score2) => score2.score - score1.score).slice(0, 10)
+    }
+
     return (
         <>
-            <h1>High Scores</h1>
+            {loading && <div id="loader"></div>}
+            {!loading &&
+                <>
+                    <h1>High Scores</h1>
+                    <div id="highScoresList">
+                        {scores.map((record) => (
+                            <li className="high-score" key={record.key}>{record.name} - {record.score}</li>
+                        ))}
+                    </div>
+                </>
+            }
         </>
     )
 }
