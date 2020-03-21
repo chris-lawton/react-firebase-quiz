135ab62144304e4b1c81b793c8eb9b48bfd633da
Chris Lawton <chris.lawton@torchbox.com>
2020-03-21 13:07:41 +0000

    re-add home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    return (
+        <>
+            <h1>Quiz App</h1>
+            <Link to="/game" className="btn">Start Game</Link>
+            <Link to="/highScores" className="btn">High Scores</Link>
+        </>
+    )
+}
